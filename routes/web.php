26c16d6b5ad1bc741d226e2a79ce7697bf4d042b
SoufianeZIANI <soufianeziani27@gmail.com>
2024-06-02 15:36:07 +0100

    chore: refactoring

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,3 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ContactController::class);
-Route::get('/test', function () {
-    dd(\App\Enums\OrganizationStatus::values());
-});
